03172e0b1b292cd9b583ccd945c1a7cb48dde900
RasmusLynge <RasmuslJacobsen@live.com>
2019-05-09 18:20:46 +0200

    new

--- src/main/java/utils/SetupTestUsers.java
+++ src/main/java/utils/SetupTestUsers.java
@@ -3,10 +3,12 @@ package utils;
 import entity.Role;
 import entity.User;
 import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
 
 public class SetupTestUsers {
 
   public static void main(String[] args) {
+//      Persistence.generateSchema("pu", null);
 
     EntityManager em = PuSelector.getEntityManagerFactory("pu").createEntityManager();
        
@@ -19,18 +21,14 @@ public class SetupTestUsers {
     em.getTransaction().begin();
     Role userRole = new Role("user");
     Role adminRole = new Role("admin");
-    User user = new User("user", "test");
+    User user = new User("user", "userpw");
     user.addRole(userRole);
-    User admin = new User("admin", "test");
+    User admin = new User("admin", "adminpw");
     admin.addRole(adminRole);
-    User both = new User("user_admin", "test");
-    both.addRole(userRole);
-    both.addRole(adminRole);
     em.persist(userRole);
     em.persist(adminRole);
     em.persist(user);
     em.persist(admin);
-    em.persist(both);
     em.getTransaction().commit();
     System.out.println("PW: " + user.getUserPass());
     System.out.println("Testing user with OK password: " + user.verifyPassword("test"));
